ac03b6ecdf033a8b393f77f50adfd0dbb8534fe1
Johnathan MEUNIER <jonathan.meunier@axa.fr>
2021-04-20 16:18:05 +0200

    display current slot

--- script/app.js
+++ script/app.js
@@ -2,3 +2,12 @@ document.querySelector("#toggleTheme").addEventListener("click", () => {
   const $body = document.querySelector("body");
   $body.dataset.theme = $body.dataset.theme === "light" ? "dark" : "light";
 });
+
+const dateObject = new Date();
+const currentDate = `${dateObject.getDate()}/${(dateObject.getMonth() + 1).toString().padStart(2, "0")}`;
+
+document.querySelectorAll("tr").forEach(($tr) => {
+  if ($tr.dataset.date === currentDate) {
+    $tr.classList.add("slot--today");
+  }
+});
