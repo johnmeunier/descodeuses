5420f64237438ebb78e0d6da86defa921318e2fa
Johnathan MEUNIER <jonathan.meunier@axa.fr>
2021-05-27 10:45:51 +0200

    affichage sous forme de boucle

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -2,11 +2,68 @@ import "./styles/reset.css";
 import "./styles/var.css";
 import "./styles/App.css";
 
+const data = [
+  {
+    date: "mardi 20 avril",
+    dateFormated: "20/04",
+    time: "9h30-12h30",
+  },
+  {
+    dateFormated: "20/04",
+    time: "13h30-17h30",
+    topic: "HTML / CSS et RWD",
+    speaker: "Johnathan MEUNIER",
+  },
+  {
+    dateFormated: "27/05",
+    date: "jeudi 27 mai",
+    time: "9h30-12h30",
+    topic: "React JS",
+    speaker: "Johnathan MEUNIER",
+  },
+  {
+    dateFormated: "27/05",
+    time: "13h30-17h30",
+    topic: "POO/UML",
+    speaker: "ES SALHI Rachida",
+  },
+  {
+    date: "lundi 21 juin",
+    dateFormated: "21/06",
+    time: "9h30-12h30",
+    topic: "JAVA",
+    speaker: "ES SALHI Rachida",
+  },
+  {
+    dateFormated: "21/06",
+    time: "13h30-17h30",
+    topic: "Cybersécurité",
+    speaker: "Tony GHILAIN",
+  },
+  {
+    date: "mercredi 7 juillet",
+    dateFormated: "07/07",
+    time: "9h30-12h30",
+    topic: "SQL",
+    speaker: "Catherine DESMOULINS",
+  },
+  {
+    time: "13h30-17h30",
+    dateFormated: "07/07",
+    topic: "UX/UI",
+    speaker: "BOY Mari",
+  },
+];
+
+const dateObject = new Date();
+const currentDate = `${dateObject.getDate()}/${(dateObject.getMonth() + 1).toString().padStart(2, "0")}`;
+
 function App() {
   return (
     <div className="App" data-theme="light">
       <div className="container">
         <h1>Intervention AXA à DesCodeuses</h1>
+
         <button id="toggleTheme">Activer dark mode</button>
 
         <table>
@@ -18,58 +75,19 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            <tr className="slot" data-date="20/04">
-              <th className="slot__date" rowSpan="2">
-                mardi 20 avril
-              </th>
-              <td className="slot__time">9h30-12h30</td>
-              <td className="slot__topic">N/D</td>
-              <td className="slot__speaker">N/D</td>
-            </tr>
-            <tr className="slot" data-date="20/04">
-              <td className="slot__time">13h30-17h30</td>
-              <td className="slot__topic">HTML / CSS et RWD</td>
-              <td className="slot__speaker">Johnathan MEUNIER</td>
-            </tr>
-            <tr className="slot" data-date="27/05">
-              <th className="slot__date" rowSpan="2">
-                jeudi 27 mai
-              </th>
-              <td className="slot__time">9h30-12h30</td>
-              <td className="slot__topic">React JS</td>
-              <td className="slot__speaker">Johnathan MEUNIER</td>
-            </tr>
-            <tr className="slot" data-date="27/05">
-              <td className="slot__time">13h30-17h30</td>
-              <td className="slot__topic">POO/UML</td>
-              <td className="slot__speaker">ES SALHI Rachida</td>
-            </tr>
-            <tr className="slot" data-date="21/06">
-              <th className="slot__date" rowSpan="2">
-                lundi 21 juin
-              </th>
-              <td className="slot__time">9h30-12h30</td>
-              <td className="slot__topic">JAVA</td>
-              <td className="slot__speaker">ES SALHI Rachida</td>
-            </tr>
-            <tr className="slot" data-date="21/06">
-              <td className="slot__time">13h30-17h30</td>
-              <td className="slot__topic">Cybersécurité</td>
-              <td className="slot__speaker">Tony GHILAIN</td>
-            </tr>
-            <tr className="slot" data-date="07/07">
-              <th className="slot__date" rowSpan="2">
-                mercredi 7 juillet
-              </th>
-              <td className="slot__time">9h30-12h30</td>
-              <td className="slot__topic">SQL</td>
-              <td className="slot__speaker">Catherine DESMOULINS</td>
-            </tr>
-            <tr className="slot" data-date="07/07">
-              <td className="slot__time">13h30-17h30</td>
-              <td className="slot__topic">UX/UI</td>
-              <td className="slot__speaker">BOY Mari</td>
-            </tr>
+            {data.map((slot) => {
+              return (
+                <tr
+                  className={`slot ${currentDate === slot.dateFormated ? "slot--today" : ""}`}
+                  data-date={slot.dateFormated}
+                >
+                  <th className="slot__date">{slot.date}</th>
+                  <td className="slot__time">{slot.time}</td>
+                  {slot.topic && <td className="slot__topic">{slot.topic}</td>}
+                  {slot.speaker && <td className="slot__speaker">{slot.speaker}</td>}
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
